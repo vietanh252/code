a13deff2f0fa92054790cc570252412213d06f4c
vietanh252 <vietanhdeptrai995@gmail.com>
2026-05-29 10:38:51 +0700

    lad3

---
+++ DBConnection.java
@@ -0,0 +1,26 @@
+package utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/company_db";
+
+    private static final String USER = "root";
+
+    private static final String PASSWORD = "15012007";
+
+    public static Connection getConnection()
+            throws Exception {
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        return DriverManager.getConnection(
+                URL,
+                USER,
+                PASSWORD
+        );
+    }
+}
